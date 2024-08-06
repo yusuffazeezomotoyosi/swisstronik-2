661fb56a213bbc19088928c929fde488212e6317
Yusuff Omotoyosi AZEEZ <azeezyusuff2017@gmail.com>
2024-08-06 21:14:34 +0000

    Initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
 
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+  const ContractFactory = await hre.ethers.getContractFactory("ZunXBT");
+  const contract = await ContractFactory.deploy(deployer.address);
 
   await contract.waitForDeployment();
 
-  console.log(`This is your deployed contract address ${contract.target}`);
+  console.log(`Your NFT Contract Address: ${contract.target}`);
 }
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
